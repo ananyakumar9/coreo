901867c2aaa9de2c3767b2113176806e32bff4ee
virtualpanda <1905228@kiit.ac.in>
2021-07-22 22:16:45 +0530

    delete acc added, data doesnt get deleted

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './Navigation.css';
-
+import firebase from '../../Config/firebase'
 const Navigation=({onRouteChange,route})=> {
   return (
     <nav className='mb4 main' >
         <div><p className='pa2 f1 ma2 b'>Coreo</p></div>
         {route==='Home'?
         <div>
-          <p 
-          onClick={()=>onRouteChange('SignIn')}
-          className='pa2 ma2 f4 link dim underline'>Sign Out</p>
-        </div>
+          
+          <p  
+    onClick={()=>{const user = firebase.auth().currentUser;
+
+    user.delete().then(() => {
+      // User deleted.
+      onRouteChange('SignIn');
+    }).catch((error) => {
+      // An error ocurred
+      // ...
+    })}}
+    className='pa2 ma2 f4 link dim underline'>delete acc</p>
+          
+          <p  
+    onClick={()=>{onRouteChange('SignIn');}}
+    className='pa2 ma2 f4 link dim underline'>Sign out</p>
+          </div>
+          
+        
         :<div className='links1'>
         <p
         onClick={()=>onRouteChange('SignIn')}
